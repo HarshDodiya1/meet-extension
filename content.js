2afe47b01bcad5d28a407fb897a14b29bfc97d00
HarshDodiya1 <dodiyaharsh999@gmail.com>
2024-03-14 17:38:01 +0530

    Added chrome extension

---
+++ content.js
@@ -0,0 +1,32 @@
+function checkJoinees(mutations) {
+    console.log("hey yaa come look at this", mutations)
+    for (const mutation of mutations) {
+      if (mutation.type === 'childList') {
+        // A child node was added or removed.
+        if (document.querySelector('div.U04fid') && document.querySelector('div.U04fid').childElementCount > 0) {
+            const name = document.querySelector('div.U04fid > img').getAttribute('title');
+            sendMessage(name);
+        }
+      }
+      else{
+        console.log("Welcome to the else part")
+      }
+    }
+    return false;
+}
+
+const observer = new MutationObserver(checkJoinees);
+observer.observe(document.body, {
+  childList: true,
+  delay: 1000
+});
+
+function sendMessage(name) {
+    const joinee = (name === null ? 'Someone' : name),
+        txt = `${joinee} has joined the call!`;
+
+    chrome.runtime.sendMessage({
+        txt,
+        action: "people_joined",
+    });
+}
